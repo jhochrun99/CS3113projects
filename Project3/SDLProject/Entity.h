7ac23bfdb88a0b47e72c704034e1a777ae22825e
Jess Hochrun <jh5367@nyu.edu>
2020-03-12 17:57:13 -0400

    Initial commit for Project 3. Contains base files, and simple code that has a single entity appear. Entity can currently by moved left and right only.

---
+++ Project3/SDLProject/Entity.h
@@ -0,0 +1,30 @@
+#pragma once
+#define GL_SILENCE_DEPRECATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+class Entity {
+public:
+    glm::vec3 position;
+    glm::vec3 movement;
+    float speed;
+
+    GLuint textureID;
+
+    glm::mat4 modelMatrix;
+
+    Entity();
+
+    void Update(float deltaTime);
+    void Render(ShaderProgram* program);
+    void DrawSpriteFromTextureAtlas(ShaderProgram* program, GLuint textureID, int index);
+};
