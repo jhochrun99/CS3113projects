e47ce0339fa6c0af09bdefa3f36cce78cb7c872a
Jess Hochrun <jh5367@nyu.edu>
2020-04-17 23:50:02 -0400

    Added 3 slime enemies, must add player collision

--- Project5/SDLProject/Level1.cpp
+++ Project5/SDLProject/Level1.cpp
@@ -2,7 +2,7 @@
 #define LEVEL1_WIDTH 21
 #define LEVEL1_HEIGHT 8
 
-#define ENEMY_COUNT 1
+#define ENEMY_COUNT 3
 
 unsigned int level1_data[] = {
     45, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 45,
@@ -24,35 +24,46 @@ void Level1::Initialize() {
 
     state.player = new Player();
 
+
+
     state.enemies = new Enemy[ENEMY_COUNT];
-    state.enemies[0].entityType = ENEMY;
-    state.enemies[0].enemy = SLIME;
-    state.enemies[0].textureID = Util::LoadTexture("slime.png");
-    state.enemies[0].textureCols = 8;
-    state.enemies[0].textureRows = 3;
-    state.enemies[0].height = 0.5f;
-    state.enemies[0].width = 0.9f;
+    GLuint slimeTextureID = Util::LoadTexture("slime.png");
+    for (int i = 0; i < ENEMY_COUNT; i++) {
+        state.enemies[i].entityType = ENEMY;
+        state.enemies[i].enemy = SLIME;
+        state.enemies[i].textureID = slimeTextureID;
 
-    state.enemies[0].animLeft = new int[8]{ 0, 1, 2, 3, 4, 5, 6, 7 };
-    state.enemies[0].animRight = new int[8]{ 8, 9, 10, 11, 12, 13, 14, 15 };
-    state.enemies[0].animUp = new int[8]{ 16, 17, 18, 19, 20, 21, 22, 23 };
-    state.enemies[0].animIndices = state.enemies[0].animLeft;
-    state.enemies[0].animFrames = 8;
+        state.enemies[i].textureCols = 8;
+        state.enemies[i].textureRows = 3;
+        state.enemies[i].height = 0.5f;
+        state.enemies[i].width = 0.9f;
 
+        state.enemies[i].animLeft = new int[8]{ 0, 1, 2, 3, 4, 5, 6, 7 };
+        state.enemies[i].animRight = new int[8]{ 8, 9, 10, 11, 12, 13, 14, 15 };
+        state.enemies[i].animUp = new int[8]{ 16, 17, 18, 19, 20, 21, 22, 23 };
+        state.enemies[i].animIndices = state.enemies[i].animLeft;
+        state.enemies[i].animFrames = 8;
+        state.enemies[i].movement = glm::vec3(-1, 0, 0);
+    }
     state.enemies[0].position = glm::vec3(6.0f, -6.0f, 0);
+    state.enemies[1].position = glm::vec3(14.0f, -5.0f, 0);
+    state.enemies[2].position = glm::vec3(18.0f, -5.0f, 0);
 }
 
 void Level1::Update(float deltaTime) {
-    state.player->Update(deltaTime, state.map, state.enemies, 0);
-    state.enemies->Update(deltaTime, state.map, NULL, 0);
+    state.player->Update(deltaTime, state.map, state.enemies, ENEMY_COUNT);
 
-    if (state.player->position.x >= 12) {
-        //state.nextScene = 1;
+    for (int i = 0; i < ENEMY_COUNT; i++) {
+        state.enemies[i].Update(deltaTime, state.map, NULL, 0);
     }
 }
 
 void Level1::Render(ShaderProgram* program) {
     state.map->Render(program);
-    state.enemies->Render(program);
+
+    for (int i = 0; i < ENEMY_COUNT; i++) {
+        state.enemies[i].Render(program);
+    }
+
     state.player->Render(program);
 }
