d59cc47db3b91ea6bcb0f72538ede9ae80aa9ad2
Jess Hochrun <jh5367@nyu.edu>
2020-04-19 04:06:25 -0400

    Altering losing message

--- Project5/SDLProject/Scene.cpp
+++ Project5/SDLProject/Scene.cpp
@@ -4,10 +4,10 @@ void Scene::EndMessage(ShaderProgram* program, bool win) {
 	GLuint fontTextureID = Util::LoadTexture("font1.png");
 	if (win) {
 		Util::DrawText(program, fontTextureID, "You Win!", 1.0f, -0.5f,
-			glm::vec3(3.5f, -2.5f, 0));
+			glm::vec3(16.5f, -3.0f, 0));
 	}
 	else {
 		Util::DrawText(program, fontTextureID, "You lose...", 1.0f, -0.5f,
-			glm::vec3(3.5f, -2.5f, 0));
+			glm::vec3(3.5f, -3.0f, 0));
 	}
 }
